77315b16f60861e3e560a755e46b4104cee3f378
Aleksei Loginov <alekseiloginov@icloud.com>
2017-04-27 10:49:02 +0300

    fetches User's relations in one query instead of n+1

--- backend/src/main/java/com/epam/ridesharing/data/repo/UserRepository.java
+++ backend/src/main/java/com/epam/ridesharing/data/repo/UserRepository.java
@@ -2,6 +2,9 @@ package com.epam.ridesharing.data.repo;
 
 import com.epam.ridesharing.data.model.Address;
 import com.epam.ridesharing.data.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -11,6 +14,11 @@ import java.util.List;
  * Custom Spring Data repository for User entity.
  */
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"home", "office", "car"})
+        // fetches relations in one query instead of n+1
+    Page<User> findAll(Pageable pageable);
 
     List<User> findByEmail(@Param("email") String email);
 
