b81fd7cce66a05becb99678f04108529f09c5e2b
Aleksei Egorov <aleksei_egorov@epam.com>
2017-05-05 09:44:20 +0300

    add Offices page stub

---
+++ frontend/src/app/admin/offices/offices.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OfficesComponent } from './offices.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        OfficesComponent
+    ],
+    declarations: [
+        OfficesComponent
+    ]
+})
+export class OfficesModule { }
