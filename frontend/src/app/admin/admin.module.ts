91198bd011ff080dd291bd7f26a054de0f7dd354
Aleksei Egorov <aleksei_egorov@epam.com>
2017-05-07 10:58:06 +0300

    guard admin views from direct access via manually entering path into address bar

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { CanActivateAdminRouteGuard } from './can-activate-admin-route.guard';
 import { TabsModule } from '../rs-components/tabs';
 import { AdminComponent } from './admin.component';
 import { OfficesModule } from './offices';
@@ -17,6 +18,8 @@ import { UsersModule } from './users';
     ],
     exports: [],
     declarations: [AdminComponent],
-    providers: [],
+    providers: [
+        CanActivateAdminRouteGuard
+    ],
 })
 export class AdminModule { }
--- frontend/src/app/admin/admin.routing.ts
+++ frontend/src/app/admin/admin.routing.ts
@@ -1,12 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 
+import { CanActivateAdminRouteGuard } from './can-activate-admin-route.guard';
 import { AdminComponent } from './admin.component';
 import { OfficesComponent } from './offices';
 import { UsersComponent } from './users';
 
 export const ADMIN_ROUTES: Routes = [
     {
-        path: 'admin', component: AdminComponent, children: [
+        path: 'admin', component: AdminComponent, canActivate: [CanActivateAdminRouteGuard], children: [
             { path: 'offices', component: OfficesComponent },
             { path: 'users', component: UsersComponent },
         ]
