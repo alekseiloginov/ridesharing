55b08cb16023fee090af17bc4ca2ffc9cdccaec3
Aleksei Loginov <alekseiloginov@icloud.com>
2017-05-09 17:18:21 +0300

    remove JSON repository populator from spring config

--- backend/src/main/java/com/epam/ridesharing/config/SpringConfiguration.java
+++ backend/src/main/java/com/epam/ridesharing/config/SpringConfiguration.java
@@ -3,15 +3,7 @@ package com.epam.ridesharing.config;
 import com.epam.ridesharing.data.model.Address;
 import com.epam.ridesharing.data.model.Car;
 import com.epam.ridesharing.data.model.User;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
@@ -24,20 +16,5 @@ public class SpringConfiguration extends RepositoryRestMvcConfiguration {
     @Override
     public RepositoryRestConfiguration config() {
         return super.config().exposeIdsFor(Address.class, User.class, Car.class).setBasePath("/api");
-    }
-
-    @Profile("DEV")
-    @Bean
-    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
-        ObjectMapper mapper = new ObjectMapper()
-                .disable(MapperFeature.USE_ANNOTATIONS)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
-        factory.setMapper(mapper);
-        Resource[] sourceData = new Resource[]{
-                new ClassPathResource("data/offices.json"),
-                new ClassPathResource("data/users.json")};
-        factory.setResources(sourceData);
-        return factory;
     }
 }
