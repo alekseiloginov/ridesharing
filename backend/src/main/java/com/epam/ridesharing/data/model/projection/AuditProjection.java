87856611aa48c25b0c0fcb1c2e7949fe520ebb01
Aleksei Loginov <alekseiloginov@icloud.com>
2017-06-03 20:40:56 +0300

    add projection to expose audit attributes, which are ignored in json

---
+++ backend/src/main/java/com/epam/ridesharing/data/model/projection/AuditProjection.java
@@ -0,0 +1,21 @@
+package com.epam.ridesharing.data.model.projection;
+
+import com.epam.ridesharing.data.model.Address;
+import com.epam.ridesharing.data.model.Car;
+import com.epam.ridesharing.data.model.User;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Date;
+
+/**
+ * Projection to expose audit attributes, which are ignored in json.
+ */
+@Projection(name = "audit", types = {User.class, Address.class, Car.class})
+public interface AuditProjection {
+
+    Long getId();
+
+    Date getCreated();
+
+    Date getModified();
+}
