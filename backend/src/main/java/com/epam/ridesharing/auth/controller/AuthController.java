72c1df0e25f9364195d54b0bf3b32888e2167ff5
Aleksei Loginov <alekseiloginov@icloud.com>
2017-07-19 00:20:12 +0300

    del LoginDTO, use spring security user instead

--- backend/src/main/java/com/epam/ridesharing/auth/controller/AuthController.java
+++ backend/src/main/java/com/epam/ridesharing/auth/controller/AuthController.java
@@ -1,12 +1,10 @@
 package com.epam.ridesharing.auth.controller;
 
 import com.epam.ridesharing.auth.service.AuthService;
-import com.epam.ridesharing.auth.uimodel.LoginDto;
 import com.epam.ridesharing.auth.uimodel.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +16,7 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("login")
-    public UserDto login(@RequestBody LoginDto loginDto) {
-        return authService.getUser(loginDto.getEmail());
+    public UserDto login() {
+        return authService.getUser();
     }
 }
--- backend/src/main/java/com/epam/ridesharing/auth/uimodel/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.epam.ridesharing.auth.uimodel;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String email;
-    private String password;
-}
