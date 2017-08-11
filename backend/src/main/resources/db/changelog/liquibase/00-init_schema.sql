deb781a8b341baed3a835997fb6365139a0f75ab
Aleksei Loginov <alekseiloginov@icloud.com>
2017-08-11 21:38:46 +0300

    cleanup

--- backend/src/main/resources/db/changelog/liquibase/00-init_schema.sql
+++ backend/src/main/resources/db/changelog/liquibase/00-init_schema.sql
@@ -2,56 +2,39 @@
 
 --changeset aloginov:1
 CREATE TABLE address (
-  id        BIGSERIAL               NOT NULL, -- (BIG)SERIAL added in H2 for PG compatibility and means AUTO_INCREMENT
+  id        BIGSERIAL PRIMARY KEY, -- (BIG)SERIAL works in H2 (for PG compatibility) and means AUTO_INCREMENT
   type      VARCHAR                 NOT NULL,
   address   VARCHAR,
   latitude  DOUBLE PRECISION        NOT NULL,
   longitude DOUBLE PRECISION        NOT NULL,
   version   BIGINT DEFAULT 0        NOT NULL,
   created   TIMESTAMP DEFAULT now() NOT NULL,
-  modified  TIMESTAMP DEFAULT now() NOT NULL,
-  CONSTRAINT address_pkey PRIMARY KEY (id)
+  modified  TIMESTAMP DEFAULT now() NOT NULL
 );
 
 --changeset aloginov:2
 CREATE TABLE app_user (
-  id               BIGSERIAL               NOT NULL,
+  id               BIGSERIAL PRIMARY KEY,
   disabled         BOOLEAN DEFAULT FALSE   NOT NULL,
   name             VARCHAR,
-  email            VARCHAR                 NOT NULL,
+  email            VARCHAR                 NOT NULL UNIQUE,
   role             VARCHAR,
   active           BOOLEAN DEFAULT TRUE    NOT NULL,
   phone            VARCHAR,
   driver           BOOLEAN DEFAULT FALSE   NOT NULL,
   in_office_hour   SMALLINT,
   from_office_hour SMALLINT,
-  office_id        BIGINT,
-  home_id          BIGINT,
+  office_id        BIGINT REFERENCES address ON DELETE SET NULL,
+  home_id          BIGINT REFERENCES address ON DELETE SET NULL,
   free_car_seats   SMALLINT,
   password         VARCHAR                 NOT NULL,
   version          BIGINT DEFAULT 0        NOT NULL,
   created          TIMESTAMP DEFAULT now() NOT NULL,
-  modified         TIMESTAMP DEFAULT now() NOT NULL,
-  CONSTRAINT app_user_pkey PRIMARY KEY (id)
+  modified         TIMESTAMP DEFAULT now() NOT NULL
 );
 
 --changeset aloginov:3
-ALTER TABLE app_user
-  ADD CONSTRAINT uk_user_email UNIQUE (email);
+CREATE INDEX ON app_user (home_id); -- foreign key idx
 
 --changeset aloginov:4
-CREATE INDEX fk_user_home_address_index
-  ON app_user (home_id);
-
---changeset aloginov:5
-CREATE INDEX fk_user_office_address_index
-  ON app_user (office_id);
-
---changeset aloginov:6
-ALTER TABLE app_user
-  ADD CONSTRAINT fk_user_home_address FOREIGN KEY (home_id) REFERENCES address (id) ON DELETE SET NULL;
-
---changeset aloginov:7
-ALTER TABLE app_user
-  ADD CONSTRAINT fk_user_office_address FOREIGN KEY (office_id) REFERENCES address (id) ON DELETE SET NULL;
-
+CREATE INDEX ON app_user (office_id); -- foreign key idx
